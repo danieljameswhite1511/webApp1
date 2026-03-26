6636da0db7ccda42a7820a0981bff3e1fcc4c9bc
Dan.White <danieljameswhite1511@gmail.com>
2026-03-26 06:56:46 +0000

    Add some more functions to the GenericRepository

--- src/Domain/Common/Repositories/IRepository.cs
+++ src/Domain/Common/Repositories/IRepository.cs
@@ -2,9 +2,12 @@
 
 namespace Domain.Common.Repositories;
 
-public interface IRepository<TEntity, TPrimaryKey> where TEntity : IEntity<TPrimaryKey> where TPrimaryKey : IEquatable<TPrimaryKey>
-{
+public interface IRepository<TEntity, TPrimaryKey> where TEntity : IEntity<TPrimaryKey> where TPrimaryKey : IEquatable<TPrimaryKey> {
     
-    Task<TEntity?> Get(TPrimaryKey id);
+    Task<TEntity?> GetAsync(TPrimaryKey id);
+    IQueryable<TEntity> GetAll();
+    TEntity Insert(TEntity entity);
+    TEntity Update(TEntity entity);
+    TEntity Delete(TEntity entity);
     
 }
--- src/Infrastructure/Persistence/GenericRepository.cs
+++ src/Infrastructure/Persistence/GenericRepository.cs
@@ -14,8 +14,31 @@ public class GenericRepository<TEntity, TPrimaryKey> : IRepository<TEntity, TPri
         _dbContext = dbContext;
     }
     
-    public async Task<TEntity?> Get(TPrimaryKey id)
+    public async Task<TEntity?> GetAsync(TPrimaryKey id)
     {
         return await _dbContext.Set<TEntity>().FirstOrDefaultAsync(x => x.Id.Equals(id));
+    }
+
+    public IQueryable<TEntity> GetAll()
+    {
+        return _dbContext.Set<TEntity>();
+    }
+
+    public TEntity Insert(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Add(entity);
+        return entity;
+    }
+
+    public TEntity Update(TEntity entity)
+    { 
+        _dbContext.Set<TEntity>().Update(entity);
+       return  entity;
+    }
+
+    public TEntity Delete(TEntity entity)
+    {
+        _dbContext.Set<TEntity>().Remove(entity);
+        return entity;
     }
 }
